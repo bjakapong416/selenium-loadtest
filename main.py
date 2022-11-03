0b055ad5ed0730dcc29b6d3f06063be1d16be443
bjakapong416 <boon.jakapong@gmail.com>
2022-11-03 16:53:01 +0700

    run test concurrent

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from selenium.webdriver.chrome.options import Options
 import threading
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+import time, os
+from multiprocessing import Pool, TimeoutError
+from threading import Thread, Barrier
 
 
 chrome_options = Options()
@@ -22,14 +25,16 @@ username = "vam01"
 password = "test01"
 
 links = ["https://vxnat.vamstack.com/vxnat"]
-handles = drivers.window_handles
+# handles = drivers.window_handles
 
 
 def open_page(url, tab_index):
+# def open_page(tab_index):
     # drivers.switch_to.window(handles[-1])
 
     if tab_index == 0:
         drivers.get(url)
+
         # drivers.get(start_url)
         # find username/email field and send the username itself to the input field
         drivers.find_element("name", "username").send_keys(username)
@@ -37,14 +42,13 @@ def open_page(url, tab_index):
         drivers.find_element("name", "password").send_keys(password)
         # click login button
         drivers.find_element("name", "login").click()
-        sleep(1)
+
     else:
 
         # Create New tab
         drivers.execute_script("window.open('');")
         # Switch to the new window
         drivers.switch_to.window(drivers.window_handles[tab_index])
-
         drivers.get(url)
         # drivers.get(start_url)
         # find username/email field and send the username itself to the input field
@@ -53,13 +57,12 @@ def open_page(url, tab_index):
         drivers.find_element("name", "password").send_keys(password)
         # click login button
         drivers.find_element("name", "login").click()
-        sleep(1)
-
     return
 
 
+
 all_threads = []
-for i in range(0, 2):
+for i in range(0, 5):
     current_thread = threading.Thread(target=open_page, args=(links[0], i,))
     all_threads.append(current_thread)
     current_thread.start()
---
+++ main_1.py
@@ -0,0 +1,116 @@
+import datetime
+import sys
+from selenium import webdriver 
+from concurrent.futures import ThreadPoolExecutor, wait
+from time import sleep, time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+# from scrapers.scraper import connect_to_base, get_driver, parse_html, write_to_file
+# VXNAT
+username = "vam01"
+password = "test01"
+num_thread = 10000
+# def page_is_loaded(drivers):
+#     print("in page_is_loaded()")
+#     return drivers.find_element("id", "API-PLAYGROUND") != None
+
+def open_page(url, number):
+# def open_page(tab_index):
+    # drivers.switch_to.window(handles[-1])
+
+    # if tab_index == 0:
+
+    chrome_options = Options()
+    #chrome_options.add_argument("--disable-extensions")
+    #chrome_options.add_argument("--disable-gpu")
+    #chrome_options.add_argument("--no-sandbox") # linux only
+    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("window-size=1900,1080")
+    drivers = webdriver.Chrome(options=chrome_options)
+    # drivers = webdriver.Chrome()
+
+    drivers.get(url)
+    # print(drivers.get(url))
+
+    # drivers.get(start_url)
+    # find username/email field and send the username itself to the input field
+    # if number == 1:
+    #     username = 'vam01'
+    #     password = 'test01'
+    # else: 
+    #     username = 'admin'
+    #     password = 'vamsuperadmin'
+
+    # print(username, password)
+    print("start time and thread num: ", number, time() )
+    drivers.find_element("name", "username").send_keys(username)
+    # print(1)
+    # find password input field and insert password as well
+    drivers.find_element("name", "password").send_keys(password)
+    # print(2)
+    # click login button
+    drivers.find_element("name", "login").click()
+    # print("finished")
+    # click first project's element
+    # drivers.find_element("id","API-PLAYGROUND").click()
+    # print("start")
+    # wait = ui.WebDriverWait(drivers, 10)
+    # wait.until(page_is_loaded)
+    # if drivers.find_element("name", "thumbnail_title") != None:
+    #     print("thumbnail is existed")
+    # # print(text)
+    # else: 
+    #     print("no thumbnail")
+    # sleep(5)
+
+    # sleep(60)
+    # else:
+
+    #     # Create New tab
+    #     drivers.execute_script("window.open('');")
+    #     # Switch to the new window
+    #     drivers.switch_to.window(drivers.window_handles[tab_index])
+    #     drivers.get(url)
+    #     # drivers.get(start_url)
+    #     # find username/email field and send the username itself to the input field
+    #     drivers.find_element("name", "username").send_keys(username)
+    #     # find password input field and insert password as well
+    #     drivers.find_element("name", "password").send_keys(password)
+    #     # click login button
+    #     drivers.find_element("name", "login").click()
+    drivers.quit()
+    return
+
+
+if __name__ == "__main__":
+
+    # headless mode?
+    headless = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "headless":
+            print("Running in headless mode")
+            headless = True
+
+    # set variables
+    start_time = time()
+    output_timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    # output_filename = f"output_{output_timestamp}.csv"
+    futures = []
+
+    url = "https://vxnat.vamstack.com/vxnat"
+    # scrape and crawl
+    with ThreadPoolExecutor() as executor:
+        for number in range(num_thread):
+            print("thread nums:", number)
+            futures.append(
+                # executor.submit(run_process, output_filename, headless)
+                executor.submit(open_page, url, number)
+            )
+
+    wait(futures)
+    # open_page(url)
+    end_time = time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed run time: {elapsed_time} seconds")
